fcb3fc3a1e88f2bbcbae031fe50556961ee54d6a
omarhuanca <oma378501@gmail.com>
2015-10-07 10:59:27 -0400

    add template: input, output, work button compile show request output textarea.

--- client/views/editor/editor.js
+++ client/views/editor/editor.js
@@ -7,15 +7,52 @@ Template.Editor.helpers({
             mode: "text/x-c++src",
             lineWrapping: true,
             cursorHeight: 1.5,
+            theme: "lesser-dark",
         }
     },
 
-    "editorCode": function() {
-        return "Code to show in editor";
+});
+
+
+Template.Editor.events({
+    'click #compile':function(event, template){
+        event.preventDefault();
+        // get property of editor
+        var text = template.find("#id_codemirror").value;
+        // verify text is no empty
+        if( text !== ""){
+            // initialize object document
+            var document = {
+                code: text,
+                language: "cpp"
+            };
+            // insert row in database
+            Documents.insert(document, function(error, documentId){
+                // verify any error
+                if(error){
+                    console.log('Document insert error', error);
+                    return;
+                }else{
+                    console.log('Document insert success');
+                    // call method
+                    Meteor.call("compile", documentId, document.language, function(error, reply){
+                        //
+                        if(error){
+                            console.log(error.reason);
+                        }else{
+                            var box_output = $('#output');
+                            box_output.val(reply);
+                            console.info(reply);
+                        }
+                    });
+                }
+            });
+
+        }
+
     },
-
-    "getEditorText": function() {
-        return Session.get("varName");
+    'click #run':function(event, template){
+        event.preventDefault();
+        console.log("run");
     }
-
 });
