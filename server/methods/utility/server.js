52612d89dab8cf245f37c9a5e098073e51a7a199
oma378501 <oma378501@gmail.com>
2015-10-13 14:33:35 -0400

    call again server compile and show output.

--- server/methods/utility/server.js
+++ server/methods/utility/server.js
@@ -4,6 +4,8 @@ if (Meteor.isServer) {
         compile: function (documentId) {
             var request = Meteor.npmRequire('request');
 
+            var idTaskExecution;
+
             var r = request.post('http://localhost:8888/api/compile', Meteor.bindEnvironment(function (err, httpResponse, body) {
 
                 var taskExecution = {
@@ -13,35 +15,36 @@ if (Meteor.isServer) {
 
                 if (err || httpResponse.statusCode !== 200) {
 
-                    taskExecution.status = 1; // success
+                    taskExecution.status = 1; // fail
 
                     TaskExecutions.insert(taskExecution, function (error, taskExecutionId) {
                         if (error) {
                             $("#error").val('');
                             $("#error").append(error);
                         }
+
+                        idTaskExecution = taskExecutionId;
                     });
 
                     console.error('error:', err || httpResponse.body);
 
                 } else {
 
-                    taskExecution.status = 0; //fail
+                    taskExecution.status = 0; //success
 
                     TaskExecutions.insert(taskExecution, function(error, taskExecutionId){
                         if (error) {
                             $("#error").val('');
                             $("#error").append(error);
                         }
+
+                        idTaskExecution = taskExecutionId;
                     });
                 }
 
             }));
 
-            var form = r.form();
-            form.append('taskExecution', taskExecutionId);
-
-            return taskExecutionId;
+            return idTaskExecution;
         },
 
     });
